30152b3b040fca3e7501ae0022853a81fdfe24da
kalpanabhatt8 <kalpanabhatt818@gmail.com>
2025-09-22 14:58:49 +0530

    -- new ui + fonts

--- src/app/landing.tsx
+++ src/app/landing.tsx
@@ -3,32 +3,41 @@ import Link from "next/link";
 
 export default function Landing() {
   return (
-    <section className="">
-      <section className="">
-        <div className="flex items-center justify-center h-[90vh]">
+    <section className="h-[100vh]">
+      <section className="flex justify-between flex-col items-center">
+        <div className="flex items-center justify-center h-[90vh] flex-col">
           <section className="w-[fit-content] h-[fit-content] container flex justify-center items-center flex-col gap-0">
-            <h1 className="pixel-font !mb-0 text-5xl text-left leading-none tracking-[0em] select-none text-black/80 font-bold">
-              KEEPS{" "}
+            {/* <h1 className=" text-5xl text-left leading-none tracking-[0em] select-none text-black/80 font-black flex gap-2 flex-col">
+           <div>   KS{" "}</div> 
+            </h1> */}
+            <h1 className=" text-5xl text-left leading-none tracking-[0em] select-none text-black/80 font-black flex gap-2 flex-col">
+           <div> <span className="text-[#eb9d00] rotate-x-7"> ✦</span> KEEPS</div> 
+           {/* <div className="grotesk-font text-sm s">KEEPS</div> */}
             </h1>
             {/* <h2 className="!mb-0 text-4xl text-black/90 font-bold" >A page for every day. </h2> */}
-            <p className="text-[1.4rem] text-black/60 font-extralight mt-[-4px] tracking-wide slab-font">
+            <p className="text-[1.15rem] text-black/60 mt-2 tracking-wide body-font ">
               Plan, rant, dream, or just collect pretty things.
+
             </p>
-            <div className="flex flex-wrap items-center gap-4 mt-8">
-              <Link href="/sign-in">
-                <button type="button" className="keeps-button">
-                  Sign In
-                </button>
+            <div className="flex flex-wrap items-center gap-8 mt-6">
+              <Link href="/sign-in" className="text-sm transition-colors">
+              <button type="button" className="button-secondary">
+               Sign - Save My Vibes
+               </button>
               </Link>
-           
-              <Link href="/sign-up">
-                <button type="button" className="keeps-button">
-                Get Started
+              <Link href="/dashboard">
+                <button type="button" className="button-primary">
+                 Try Keeps Now ✨
                 </button>
               </Link>
             </div>
+           
           </section>
+
         </div>
+          <p className="text-xs text-black/40 mt-4 ">
+              All your vibes stay on this device. <Link href="/sign-up" className="underline-offset-2 hover:underline text-black/60 hover:text-black/80 transition-colors">Make an account</Link> when you’re ready to keep them safe everywhere.
+            </p>
       </section>
     </section>
   );
